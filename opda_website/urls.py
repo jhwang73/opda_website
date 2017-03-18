ad73c244967c629a046e3644bf677242c6ba2518
Jason Hwang <jasonhwang97@gmail.com>
2017-03-18 01:57:10 -0400

    Made some progress with the views, i'm pretty sure it shouldnt be in the profiles app but i guess it doesn't matter

--- opda_website/urls.py
+++ opda_website/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from profiles import views as profile_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', profile_views.home, name='home'),
 ]
