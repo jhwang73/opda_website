6913c3b3a1147f5787bd7003cf018aa9470a8f10
Jason Hwang <jasonhwang97@gmail.com>
2017-03-18 15:45:41 -0400

    added the shop homepage template and moved the template folder back into profiles app cuz i didnt know how to deal with it.

--- profiles/views.py
+++ profiles/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {}
-    template = 'HTML here'
+    template = 'index.html'
     return render(request,template,context)
